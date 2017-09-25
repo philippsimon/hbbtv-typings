8bcc88e6b51b8e426558c44de2e3ff4377e40f38
Philipp Simon Schmidt <philippsimon@users.noreply.github.com>
2017-09-25 16:01:15 +0200

    add references to all types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,32 @@
+/// <reference path="./types/objects/ApplicationManagerObject.d.ts" />
+/// <reference path="./types/objects/CapabilitiesObject.d.ts" />
+/// <reference path="./types/objects/CodManagerObject.d.ts" />
+/// <reference path="./types/objects/ConfigurationObject.d.ts" />
+/// <reference path="./types/objects/DownloadManagerObject.d.ts" />
+/// <reference path="./types/objects/DownloadTriggerObject.d.ts" />
+/// <reference path="./types/objects/DrmAgentObject.d.ts" />
+/// <reference path="./types/objects/GatewayInfoObject.d.ts" />
+/// <reference path="./types/objects/MDTFObject.d.ts" />
+/// <reference path="./types/objects/NotifSocketObject.d.ts" />
+/// <reference path="./types/objects/ParentalControlManagerObject.d.ts" />
+/// <reference path="./types/objects/RecordingSchedulerObject.d.ts" />
+/// <reference path="./types/objects/RemoteManagementObject.d.ts" />
+/// <reference path="./types/objects/SearchManagerObject.d.ts" />
+/// <reference path="./types/objects/StatusViewObject.d.ts" />
+/// <reference path="./types/objects/VideoBroadcastObject.d.ts" />
+/// <reference path="./types/objects/VideoMpegObject.d.ts" />
+/// <reference path="./types/objects/RemoteManagementObject.d.ts" />
+/// <reference path="./types/Application.d.ts" />
+/// <reference path="./types/ApplicationCollection.d.ts" />
+/// <reference path="./types/ApplicationPrivateData.d.ts" />
+/// <reference path="./types/Configuration.d.ts" />
+/// <reference path="./types/Keyset.d.ts" />
+/// <reference path="./types/LocalSystem.d.ts" />
+/// <reference path="./types/NetworkInterface.d.ts" />
+/// <reference path="./types/StringCollection.d.ts" />
+/// <reference path="./types/Window.d.ts" />
+/// <reference path="./types/oipfObjectFactory.d.ts" />
+
 /**
  * @see http://www.etsi.org/deliver/etsi_ts%5C102700_102799%5C102796%5C01.02.01_60%5Cts_102796v010201p.pdf
  * 
