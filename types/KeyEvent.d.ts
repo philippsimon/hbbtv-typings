587f0e95ec1c1b221b5afc1463567244ac47b222
philippsimon <github@philippsimon.de>
2018-01-05 14:37:28 +0100

    update KeyEvent

--- types/KeyEvent.d.ts
+++ types/KeyEvent.d.ts
@@ -41,30 +41,161 @@ interface KeyEventInit extends EventModifierInit {
 declare var KeyEvent: {
   prototype: KeyEvent;
   new(typeArg: 'keydown' | 'keypress' | 'keyup', eventInitDict?: KeyEventInit): KeyEvent;
-  readonly VK_RED: number;
-  readonly VK_GREEN: number;
-  readonly VK_YELLOW: number;
-  readonly VK_BLUE: number;
-  readonly VK_LEFT: number;
-  readonly VK_UP: number;
-  readonly VK_RIGHT: number;
-  readonly VK_DOWN: number;
-  readonly VK_ENTER: number;
+
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_0: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_1: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_2: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_3: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_4: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_5: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_6: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_7: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_8: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_9: number;
-  readonly VK_PLAY: number;
-  readonly VK_PAUSE: number;
-  readonly VK_STOP: number;
-  readonly VK_FAST_FWD: number;
-  readonly VK_REWIND: number;
+
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
+  readonly VK_UP: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
+  readonly VK_DOWN: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
+  readonly VK_LEFT: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
+  readonly VK_RIGHT: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
+  readonly VK_ENTER: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=234
+   */
   readonly VK_BACK: number;
+
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_RED: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_GREEN: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_YELLOW: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_BLUE: number;
+
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_PLAY: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_PAUSE: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_STOP: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_NEXT: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_PREV: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_PLAY_PAUSE: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_FAST_FWD: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_REWIND: number;
+
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_HOME: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_MENU: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_GUIDE: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
   readonly VK_TELETEXT: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_SUBTITLES: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_CHANNEL_UP: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_CHANNEL_DOWN: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_VOLUME_UP: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_VOLUME_DOWN: number;
+  /**
+   * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=235
+   */
+  readonly VK_MUTE: number;
 };
