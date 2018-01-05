1f79c0b88b0c21a1b477f4ac1cf3ceec73e850de
philippsimon <github@philippsimon.de>
2018-01-05 14:08:27 +0100

    fix Window.postMessage

--- types/Window.d.ts
+++ types/Window.d.ts
@@ -1,10 +1,12 @@
-declare namespace OIPF {
+interface Window {
     /**
-     * A strict subset of the DOM Window object representing the application. No symbols from the Window
-     * object are accessible through this property except `postMessage`: 
+     * Cross-document messaging, as defined in section 8.2 of [HTML5], a subset. The client SHALL
+     * support posting messages with the postMessage method as defined in chapter 8.2.3 of [HTML5],
+     * prototype also listed below for reference.The MessageEvent interface defined in 8.1 of
+     * [HTML5] SHALL be supported, except for the ports value which MAY be undefined if the
+     * client does not support passing messages with ports. 
+     * 
+     * @see http://www.oipf.tv/docs/oipf-archive/OIPF-T1-R1-Specification-Volume-5-Declarative-Application-Environment-V1_1-2009-10-08.pdf#page=256
      */
-    export interface Window {
-        postMessage( message: any, targetOrigin: string ): void; 
-    }
-
+    postMessage( message: any, targetOrigin: string ): void; 
 }
