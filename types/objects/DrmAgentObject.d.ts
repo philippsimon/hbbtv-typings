5c294f794317095cfddb94f16577f0b9c1cc32bc
Matt Stevens <matt@vodlogic.com>
2022-03-23 12:08:53 +0000

    feat: Adds setActiveDRM() method to drmAgent as specified in section A.2.27 of HBBTV 2.0.3

--- types/objects/DrmAgentObject.d.ts
+++ types/objects/DrmAgentObject.d.ts
@@ -23,6 +23,30 @@ declare namespace OIPF {
         DRMSystemStatus(DRMSystemID: string): DRMSystemStatusValue;
         canPlayContent(DRMPrivateData: string, DRMSystemID: string): boolean;
         canRecordContent(DRMPrivateData: string, DRMSystemID: string): boolean;
+
+        /**
+         * Sets the DRM system, specified by DRMSystemID, that the terminal shall use with any new requests for playing
+         * protected broadband content. Any other DRM systems present in the terminal shall not be used with new
+         * requests until this method is called again or the application stops for any reason, even if this means
+         * playback of content fails.
+         *
+         * If the method is called with the DRMSystemID set to null, the algorithm used by the terminal to determine
+         * which DRM to use is outside the scope of the present document. The value true shall always be returned in
+         * this case. This shall be the default state if no calls to this method have been made.
+         *
+         * A call to this method with DRMSystemID set to "urn:hbbtv:oipfdrm:inactive" shall disable the use of all DRM
+         * systems in response to requests to play protected broadband content with the exception that the operation of
+         * the EME API is not affected (see clause B.3). Methods on the oipfDrmAgent object may still be called in this
+         * state, though depending on the DRM system, some uses of sendDRMMessage may fail. Protected broadband content
+         * may still play if suitable keys or licences are provided using the EME API.
+         *
+         * If for any reason the terminal is unable to set the specified DRM system as requested, the method shall
+         * return false, otherwise it shall return true.
+         *
+         * @param drmSystemId The DRM system as defined in clause 9.3.10 of the OIPF DAE specification [1] and in Table
+         * 9 ("DRMControlInformation Type Semantics") of the OIPF Metadata specification [18].
+         */
+        setActiveDRM(drmSystemId?: string): boolean;
     }
 
 }
