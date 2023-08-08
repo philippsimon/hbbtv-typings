c22e6fe7c00948a026047145a53880f807748e02
“Ben <ben.king@freesat.co.uk>
2023-08-08 10:13:23 +0100

    Adding readonly variables to OpApp.VideoBroadcastObject interface to prevent: Type '<x>' does not satisfy the constraint 'keyof VideoBroadcastObject'

--- types/opapp/objects/VideoBroadcastObject.d.ts
+++ types/opapp/objects/VideoBroadcastObject.d.ts
@@ -2,5 +2,13 @@ declare namespace OpApp {
     // @ts-ignore
     export interface VideoBroadcastObject extends OIPF.VideoBroadcastObject {
         onChannelChangeSucceeded: ( channel: OIPF.Channel, viewerChannel: OIPF.Channel, quiet: number ) => void;
+        readonly playState: playState;
+        readonly playStateError: playStateError;
+        readonly playSpeed: Pick<VideoBroadcastObject, "playSpeed">;
+        readonly playPosition: Pick<VideoBroadcastObject, "playPosition">;
+        readonly playbackOffset: Pick<VideoBroadcastObject, "playbackOffset">;
+        readonly maxOffset: Pick<VideoBroadcastObject, "maxOffset">;
+        readonly timeShiftMode: Pick<VideoBroadcastObject, "timeShiftMode">;
+        readonly currentTimeShiftMode: Pick<VideoBroadcastObject, "currentTimeShiftMode">;
     }
 }
